8fdcdba6d81bf98c454ddd45524068f47ee8c11a
Till Ganster <tillganster@Tills-MBP.fritz.box>
2022-10-05 20:26:49 +0200

    feat: add gh cli autcompletion

--- configure.sh
+++ configure.sh
@@ -150,20 +150,24 @@ install_zsh() {
 }
 
 install_kubectl() {
-  $(curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl)
-  chmod +x ./kubectl
-  sudo mv ./kubectl /usr/local/bin/kubectl
-  kubectl version --client
+  #$(curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl)
+  #chmod +x ./kubectl
+  #sudo mv ./kubectl /usr/local/bin/kubectl
+  #kubectl version --client
+  brew list kubectl || brew install kubectl 
 }
 
 install_helm(){
-  $(curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash)
+   brew list helm  || brew install helm  
+ #$(curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash)
 }
 
 install_kops(){
-  $(curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64)
-  chmod +x ./kops
-  sudo mv ./kops /usr/local/bin/
+  #$(curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64)
+  #chmod +x ./kops
+  #sudo mv ./kops /usr/local/bin/
+  
+  echo "installing kops - NOT"
 }
 
 link_file() {
@@ -234,7 +238,12 @@ if [[ $BUILD ]]; then
     brew install go 
     brew install derailed/k9s/k9s
     brew install node
+    brew install fzf
+    # TODO install k8s fzf plugin
     brew install gh
+    gh completion --shell zsh > $ZSH_CUSTOM/plugins/gh.zsh
+    #echo "# github" >> ~/.zshrc.local
+    #echo "compctl -K _gh gh
      
   fi
   if [  ! -f /usr/local/bin/kubectl ]; then
